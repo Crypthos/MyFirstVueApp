e7a59b5972e047d17d6f3ba03d1e9ab137559265
Dalvin Newland <newlank@outlook.com>
2022-03-28 18:11:49 +0200

    init vue3+vite

---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>About: My First Vue3 App</h1>
+  </div>
+  <div>
+    <p v-show="inStock">In Stock</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+
+
+</script>
